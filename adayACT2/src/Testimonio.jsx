392e7c361f0637906a8e967d296778423b366177
ametralletor <adaysg14@gmail.com>
2025-11-01 13:11:35 +0000

    Arreglado el botón de like

--- adayACT2/src/Testimonio.jsx
+++ adayACT2/src/Testimonio.jsx
@@ -1,23 +1,30 @@
-import React, { useState } from "react"
-import * as React from 'react';
+
+import React, { useState } from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import CardActionArea from '@mui/material/CardActionArea';
+import IconButton from '@mui/material/IconButton';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 
 function Testimonio({id, nombre, pais, trabajo, empresa, testimonio, imagen}) {
     const [like, setLiked] = useState(false);
 
     return (
-        <Card sx={{maxWidth: 600}}>
+        <Card sx={{maxWidth: 1000, margin: '20px auto', mb:3}}>
             <CardActionArea>
-                <CardMedia component="img" height="200" image={imagen} alt={`Imagen de ${nombre}`} />
+                <CardMedia component="img" image={imagen} alt={`Imagen de ${nombre}`} sx={{
+                    width: 500,
+                    height: 500,
+                    mx: 'auto'
+                }} />
                 <CardContent>
                     <Typography gutterBottom variant="h5" component="div">{nombre} de {pais}</Typography>
                     <Typography variant="body2" sx={{color: 'text.secondary'}}>{trabajo} en {empresa}</Typography>
                     <Typography variant="body1" sx={{marginTop: 2}}>"{testimonio}"</Typography>
-                    <IconButton onClick={() => setLiked(!liked)}>
+                    <IconButton onClick={() => setLiked(!like)}>
                         {like ? <FavoriteIcon color="error" /> : <FavoriteBorderIcon />}
                     </IconButton>
                 </CardContent>
